55211d3ef1a7703b68ce1857be479f2a7946a366
Jan Schill <janschill@me.com>
2020-11-19 20:28:13 +0100

    WIP try out solid-node-client

--- code/solid-simple-client/src/index.js
+++ code/solid-simple-client/src/index.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+import { SolidNodeClient } from 'solid-node-client';
+import * as $rdf from 'rdflib';
+const client = new SolidNodeClient();
+const store = $rdf.graph();
+const fetcher = $rdf.fetcher(store, { fetch: client.fetch.bind(client) });
+// now all rdflib methods support file:// requests in nodejs
+
+async function main() {
+  let session = await login();  // may be omitted if you don't need authentication
+  // now all rdflib methods support authenticated http: requests in nodejs
+}
